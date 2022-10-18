4ef6901cb8a391b6391a13c15072dbe218aba1ce
Malonju <malonjulaure@gmail.com>
2022-10-19 00:41:07 +0200

    rajout de la suite des contrainte de la table job_history,applicant, jobs

--- TABLES/CONTRAINTS/comments.sql
+++ TABLES/CONTRAINTS/comments.sql
@@ -56,3 +56,65 @@ comment on column offer.email
 
   comment on column offer.gender 
   is 'to determine the sex of the offer';
+
+  PROMPT creating comments jobs by malonju
+
+-- comment on table jobs
+comment on table jobs
+is 'details on an applicant job ';
+
+comment on column jobs. job_id
+is 'Auto-incrementing primary key';
+
+comment on column jobs. job_title
+is 'it is  the  name of work';
+
+PROMPT creating comments applicant by malonju
+--comment on applicant
+comment on table applicant
+is 'detail or information on an applicant ';
+
+comment on column applicant .aplicant_id 
+is 'Auto-incrementing primary key';
+
+comment on column  applicant.first_name 
+is ' the name of the applicant';
+
+comment on column  applicant.last_name 
+is ' the applicant surname ';
+
+comment on column applicant.phone_number 
+is 'the phone number of the applicant';
+
+comment on column applicant.email
+  is 'The email address that an applicant will used to access his/her account';
+
+  comment on column applicant.gender 
+  is 'to determine the sex of the applicant';
+
+  comment on column applicant . location_id 
+  is ' to locate the applicant';
+  
+  comment on column applicant.statut
+  is 'gives us imformation on his actual occupation';
+
+  comment on column applicant.job_id
+  is 'Auto_incrementing forein key refer to table jobs';
+
+PROMPT creating comments job_history by malonju
+-- Comment of job_history
+
+comment on the table job_history
+is 'for ressence the previous works of applicant ';
+
+comment on column job_history . job_history_id
+is 'Auto-incrementing primary key';
+
+comment on culumn job_history.starts_date
+is "it's date when the works bigins";
+
+comment on culumn job_history.end_date
+is "it's shows us when the applicant end his/her previous work";
+
+comment on column job_history.applicant_id
+  is 'Auto_incrementing forein key refer to table applicant';
