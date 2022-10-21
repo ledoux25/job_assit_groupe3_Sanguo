8d47ec9cb2767d27511f33cb050b50ce78ad9b1c
kanteumaxime <anangamax@gmail.com>
2022-10-21 05:30:16 +0200

    insertion des element de la table locationsans

---
+++ TABLES/Locations.sql
@@ -0,0 +1,15 @@
+PROMT INSERTING into LOCATIONS
+set define off
+begin
+INSERT INTO locations(location_id,city)  VALUE(1,'Douala');
+INSERT INTO locations(location_id,city)  VALUE(2,'Yaounde');
+INSERT INTO locations(location_id,city)  VALUE(3,'Limbe');
+INSERT INTO locations(location_id,city)  VALUE(4,'kumba');
+INSERT INTO locations(location_id,city)  VALUE(5,'kribi');
+INSERT INTO locations(location_id,city)  VALUE(6,'Bangangte');
+INSERT INTO locations(location_id,city)  VALUE(7,'loum');
+INSERT INTO locations(location_id,city)  VALUE(8,'Bertoua');
+INSERT INTO locations(location_id,city)  VALUE(9,'Mbanga');
+INSERT INTO locations(location_id,city)  VALUE(10,'Nkonssamba');
+INSERT INTO locations(location_id,city)  VALUE(11,'Tschang');
+INSERT INTO locations(location_id,city)  VALUE(11,'Bafia');
