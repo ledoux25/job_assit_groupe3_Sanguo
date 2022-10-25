9ac605019fa0ecb3b9906bf9a787ac9c45a767fe
Ledoux Sanguo <sanguojoseph17@gmail.com>
2022-10-25 06:03:24 +0100

    insertion des proposer

--- TABLES/level.sql
+++ TABLES/level.sql
@@ -1,9 +1,9 @@
 PROMPT INSERTING into LEVEL
 set define off
 begin
-insert into LEVELS(level_id,label,price) VALUES ('GRA','GRATUIT',0);
-insert into LEVELS(level_id,label,price) VALUES ('STD','STANDART',1000);
+insert into LEVELS(level_id,label,price) VALUES ('GRAT','GRATUIT',0);
+insert into LEVELS(level_id,label,price) VALUES ('STAN','STANDART',1000);
 insert into LEVELS(level_id,label,price) VALUES ('PREM','PREMIUM',25000);
-insert into LEVELS(level_id,label,price) VALUES ('DIA','DIAMANT',50000);
+insert into LEVELS(level_id,label,price) VALUES ('DIAM','DIAMANT',50000);
 end;
 /
