4445e458dff31693a2ac941ba8d45ae5539864bd
Toko Michel <tokowouendeu@gmail.com>
2022-11-04 21:15:46 +0100

    Update de la table job_done

--- TABLES/jobs_done.sql
+++ TABLES/jobs_done.sql
@@ -1,4 +1,5 @@
 SET DEFINE OFF
 PROMPT INSERTING JOBS_DONE
 begin
-INSERT INTO JOBS_DONE(job_done_id, start_date, end_date, comments, notation, offer_id) VALUES (1,to_timestamp('10-01-2022 15.12.55.124512'), null, 'sait travailler en equipe',2)
+INSERT INTO JOBS_DONE(job_done_id, start_date, end_date, comments, notation, offer_id) VALUES (1,'10/01/2022', null, 'sait travailler en equipe',2)
+--Kanteu note que le end date peut etre null lorsque tu lis le commentaire ci supprime ca du fichier...
