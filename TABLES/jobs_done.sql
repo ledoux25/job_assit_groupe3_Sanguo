09ee2c80b4b22c28e7f93473ee730e1e2ecb84c7
Toko Michel <tokowouendeu@gmail.com>
2022-11-04 21:07:43 +0100

    enregistrment des job_done

--- TABLES/jobs_done.sql
+++ TABLES/jobs_done.sql
@@ -1,4 +1,4 @@
 SET DEFINE OFF
 PROMPT INSERTING JOBS_DONE
 begin
-INSERT INTO JOBS_DONE();
+INSERT INTO JOBS_DONE(job_done_id, start_date, end_date, comments, notation, offer_id) VALUES (1,to_timestamp('10-01-2022 15.12.55.124512'), null, 'sait travailler en equipe',2)
