a9d8bd46abbbad3ba93fae4860940e85106082ad
Malonju <malonjulaure@gmail.com>
2022-11-05 09:26:21 +0100

    ajout de la propriete salaire dans la table offers

--- TABLES/CONTRAINTS/tables.sql
+++ TABLES/CONTRAINTS/tables.sql
@@ -85,7 +85,7 @@ CREATE TABLE OFFERS(
     sector_id VARCHAR(5),
     status varchar(10 char) not null,
     death_line DATE
-    salaire number(80) not null,
+    salaire number(80)not null
 );
 
 PROMPT Creating table LOCATIONS
