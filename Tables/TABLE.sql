4df8de43879b20e2e7c6b2f718ac45618c4b863e
Patrice <Wapalu2004@gmail.com>
2022-11-10 09:23:34 +0100

    Correction des erreurs dans le fichier table TABLE

--- Tables/TABLE.sql
+++ Tables/TABLE.sql
@@ -1,37 +1,37 @@
 
 PROMPT DEleting existing tables
-PROMPT Deleting existing USERS
-DROP TABLE USERS cascade constraints;
+PROMPT Deleting existing users
+DROP TABLE users cascade constraints;
 
 
-PROMPT Deleting existing CANDIDATURE 
-DROP TABLE  CANDIDATURE cascade constraints;
+PROMPT Deleting existing candidature 
+DROP TABLE  candidature cascade constraints;
 
 
 PROMPT Deleting existing PUBLICATION 
-DROP TABLE PUBLICATION cascade constraints;
+DROP TABLE publication cascade constraints;
 
 
 
-PROMPT Deleting existing BESOIN_REALISER
-DROP TABLE BESOIN_REALISER cascade constraints;
+PROMPT Deleting existing besoin_realise
+DROP TABLE besoin_realise cascade constraints;
 
 
-PROMPT Deleting existing RECOMMENDATION
-DROP TABLE RECOMMENDATION cascade constraints;
+PROMPT Deleting existing recommendation
+DROP TABLE recommendation cascade constraints;
 
 
-PROMPT deleting existing SECTORS
-DROP TABLE SECTORS cascade constraints;
+PROMPT deleting existing secteurs
+DROP TABLE secteur cascade constraints;
 
 
-PROMPT deleting existing USERS_HAS_SECTOR
-DROP TABLE USERS_HAS_SECTOR
+PROMPT deleting existing users_HAS_SECTOR
+DROP TABLE liste_secteur
  cascade constraints;
 
-PROMPT Creating table USERS
+PROMPT Creating table users
 CREATE sequence user_seq START WITH 1;
-CREATE TABLE USERS(
+CREATE TABLE users(
     user_id integer DEFAULT user_seq.NEXTVAL NOT NULL,
     first_name VARCHAR(45 CHAR), 
     last_name VARCHAR(45 CHAR),
@@ -41,9 +41,9 @@ CREATE TABLE USERS(
     status VARCHAR(15 CHAR),
     profession VARCHAR(35 CHAR)
 );
-PROMPT Creating table CANDIDATURE 
+PROMPT Creating table candidature 
 CREATE sequence can_seq START WITH 1;
-CREATE TABLE CANDIDATURE(
+CREATE TABLE candidature(
     candidature_id integer DEFAULT can_seq.NEXTVAL NOT NULL,
     date_candidature DATE ,
     status_candidature VARCHAR(15 CHAR),
@@ -52,10 +52,10 @@ CREATE TABLE CANDIDATURE(
     user_id VARCHAR(30),
     besoin_rea_id VARCHAR(45)
 );
-PROMPT Creating table PUBLICATION
+PROMPT Creating table publication
 CREATE sequence pub_seq START WITH 1;
-CREATE TABLE PUBLICATION(
- publication_id integer DEFAULT pub_seq.NEXTVAL NOT NULL,
+CREATE TABLE publication(
+publication_id integer DEFAULT pub_seq.NEXTVAL NOT NULL,
 publication_date DATE ,
 salaire float ,
 death_line VARCHAR(35),
@@ -66,18 +66,18 @@ besoin_id VARCHAR(30)
 );
 
 
-PROMPT Creating table BESOIN_REALISER
+PROMPT Creating table besoin_realise
 CREATE sequence be_rea_seq START WITH 1;
-CREATE TABLE BESOIN_REALISER(
+CREATE TABLE besoin_realise(
  besoin_realise_id integer DEFAULT be_rea_seq.NEXTVAL NOT NULL,
     note float ,
     commentaire VARCHAR( 50 CHAR)
 );
 
-CREATE TABLE RECOMMENDATION
-PROMPT Creating table 
+
+PROMPT Creating table recommendation
 CREATE sequence recomm_seq START WITH 1;
-CREATE TABLE RECOMMENDATION(
+CREATE TABLE recommendation(
     recommendation_id integer DEFAULT recomm_seq.NEXTVAL NOT NULL,
     recom_name VARCHAR(40 CHAR),
     recom_number INT,
@@ -87,19 +87,20 @@ CREATE TABLE RECOMMENDATION(
     user_id VARCHAR(30 CHAR)
 );
 
-PROMPT Creating table SECTORS
+PROMPT Creating table secteurs
 CREATE sequence sec_seq START WITH 1;
-CREATE TABLE SECTORS(
+CREATE TABLE secteurs(
     sector_id integer DEFAULT sec_seq.NEXTVAL NOT NULL,
     sector_name VARCHAR(40 CHAR),
     sector_description VARCHAR( 30 CHAR),
-    nombre_applicant INT
+    number_applicant INT
 );
-PROMPT Creating table USERS_HAS_SECTOR
+
+PROMPT Creating table liste_secteur
 CREATE sequence use_sec_seq START WITH 1;
-CREATE TABLE SECTOR(
-    user_sec_id integer DEFAULT use_sec_seq.NEXTVAL NOT NULL,
-    sector VARCHAR (30 CHAR)
+CREATE TABLE liste_secteur(
+    liste_secteur_id integer DEFAULT use_sec_seq.NEXTVAL NOT NULL,
+    secteur VARCHAR (30 CHAR)
 );
 
 
