bf92b402b0c48bfa83941ff914e96127be54b7c0
Ledoux Sanguo <sanguojoseph17@gmail.com>
2022-11-11 23:03:00 +0100

    correction des erreur dans le fichier comments

--- Tables/CONTRAINTS/comments.sql
+++ Tables/CONTRAINTS/comments.sql
@@ -1,185 +1,182 @@
 PROMPT creating comments users by CHIEMO WANDJI
 
-comment on table users
-is "detail sur l'utilisateur";
+comment on table utilisateurs
+is 'detail sur lutilisateur';
 
-comment on column users.users_id
+comment on column utilisateurs.user_id
 is ' Auto-incrémentation de la cle primaire de user';
 
-comment on column users.name
+comment on column utilisateurs.nom
 is 'nom de user';
 
-comment on column users.prenom
+comment on column utilisateurs.prenom
 is 'last-name de user';
 
-comment on column users.numero
+comment on column utilisateurs.numero
 is 'numero de telephone user';
 
-comment on column users.email
+comment on column utilisateurs.email
 is 'email de user';
 
-comment on column users.password_reset
+comment on column utilisateurs.mot_de_passe
 is 'Mot de passe de user';
 
-comment on column users.profession
+comment on column utilisateurs.profession
 is 'profession de user';
 
-comment on column users.statut
+comment on column utilisateurs.statut
 is 'statut de user';
 
 
 
 PROMPT creating comments CANDIDATURE by CHIEMO WANDJI
-comment on table candidature
+comment on table candidatures
 is 'Description de la table candidature'
 
-comment on column candidature.liste_id
+comment on column candidatures.candidature_id
 is ' Auto-incrémentation de la clé primaire';
 
-comment on column candidature.date_candidature
+comment on column candidatures.date_candidature
 is 'Date de poste de la candidature';
 
-comment on column candidature.statut
+comment on column candidatures.statut_candidature
 is ' Statut de la candidature';
 
-comment on column candidature.proposition
+comment on column candidatures.proposition
 is 'Detail sur Proposition de la proposition de candidature';
 
-comment on column publication.publication_id
+comment on column candidatures.publication_id
 is ' Auto-incrémentation de la clé primaire';
 
-comment on column users.users_id
+comment on column candidatures.user_id
 is ' Identifiant de user';
 
-comment on column besoin_realise.besoin_realise_id
-is ' Identifiant des besoins realises';
 
 
 PROMPT creating comments PUBLICATION by CHIEMO WANDJI
 
-Comment on table publication
+Comment on table publications
 is 'Donne des details sur la publication';
 
-comment on column publication.proposer_id 
+comment on column publications.publication_id 
 is 'Auto-incrementation de la cle primaire';
 
-comment on column  publication.publication_date 
+comment on column  publications.publication_date 
 is ' Date de publication de la candidature';
 
-comment on column  publication.salary 
+comment on column  publications.salaire
 is ' salaire propose dans la publication ';
 
-comment on column publication.phone_number 
-is 'Numero de telephone inscrit sur la publication';
-
-comment on column publication.email
-  is 'Email inscrit sur la publication';
-
-comment on column publication.deathline 
+comment on column publications.death_line 
 is 'Date limite sur la publication';
 
-comment on column publication.duree
+comment on column publications.duree
 is ' Duree de la publication';
 
-comment on column publication.status 
+comment on column publications.statut_publication
 is ' Statut de la publication';
 
-comment on column publication.description
+comment on column publications.description
 is ' Description de la publication';
 
-comment on column besoin.besoin_id
+comment on column publications.besoin_id
 is ' Identifiant du besoin de la publication';
 
 
 PROMPT creating comments BESOIN by CHIEMO WANDJI
-comment on table besoin
+comment on table besoins
 is 'Differrent besoin de users';
 
-comment on column besoin.besoin_id
+comment on column besoins.besoin_id
 is 'Auto-incrementation de la cle primaire';
 
-comment on column besoin_realise.description
-is 'Description des besoins';
 
-comment on column besoin_realise.paiement_mode
+comment on column besoins.paiement_par
 is ' Mode de paiement du besoin';
 
-comment on column users.users_id
+comment on column besoins.user_id
 is ' Identifiant de users';
 
-comment on column besoin.localisation
+comment on column besoins.localisation
 is ' Localisation / Emplacement du besoin';
 
-comment on column secteur.secteur_id
+comment on column besoins.secteur_id
 is ' Identifiant du secteur';
 
 
 PROMPT creating comments besoin_realise by CHIEMO WANDJI
-comment on table besoin_realise
+comment on table besoin_realiser
 is 'Differrent besoin_realise avaible';
 
-comment on column besoin_realise.besoin_realise_id
+comment on column besoin_realiser.besoin_realiser_id
 is 'Auto-incrementation de la cle primaire';
 
-comment on column besoin_realise.note
+comment on column besoin_realiser.note
 is 'Note du besoin_realise';
 
-comment on column besoin_realise.commentaire
+comment on column besoin_realiser.commentaire
 is ' Commentaire du besoin_realise';
+
+comment on column besoin_realiser.candidature_id
+is 'reference de la candidature realiser';
 
 
 
 
 PROMPT creating comments RECOMMANDATION by CHIEMO WANDJI
-comment on table recommandation
+comment on table recommendations
 is 'detail or information on an recommandation ';
 
-comment on column recommandation.recommandation_id 
+comment on column recommendations.recommendation_id 
 is 'Auto-incrementing primary key';
 
-comment on column  recommandation.first_name 
+comment on column  recommendations.recom_nom 
 is ' the name of the recommandation';
 
-comment on column  recommandation.last_name 
+comment on column  recommendations.recom_numero
 is ' the recommandation surname ';
 
-comment on column recommandation.phone_number 
-is 'the phone number of the recommandation';
 
-comment on column recommandation.email
+comment on column recommendations.recom_email
 is 'The email address of an recommandation ';
 
-comment on column recommandation.gender 
+comment on column recommendations.recom_contenue 
 is 'to determine the sex of the recommandation';
   
-comment on column recommandation.status
+comment on column recommendations.recom_statut
 is 'to if te recommandation is free or occupied';
+
+comment on column recommendations.user_id
+is 'reference de lutilisateur recommander ';
 
 
 PROMPT creating comments secteur by CHIEMO WANDJI
 
-comment on  table secteur
+comment on  table secteurs
 is 'for ressence the previous works of recommandation ';
 
-comment on column secteur.secteur_id
+comment on column secteurs.secteur_id
 is 'Auto-incrementation de la cle primaire';
 
-comment on column secteur.secteur_name
+comment on column secteurs.secteur_nom
 is 'Nom du secteur';
 
-comment on column secteur.secteur_description
+comment on column secteurs.secteur_description
 is 'Description du secteur';
 
-comment on column secteur.nombre_applicant
-is  "Nombre d'applicant du secteur";
+comment on column secteurs.nombre_utilisateur
+is  'Nombre dapplicant du secteur';
 
 
 PROMPT creating comments liste_secteur by CHIEMO WANDJI
-comment on TABLE liste_secteur
+comment on TABLE liste_secteurs
 is 'Liste des differents secteur dans lesquels users pourrait travailler';
 
-comment on column users.users_id
+comment on column liste_secteurs.user_id
 is 'Identifiant de user';
 
-comment on column secteur.secteur_id
+comment on column liste_secteurs.liste_secteur_id
+is 'Identifiant des secteur';
+
+comment on column liste_secteurs.secteur_id
 is 'Identifiant des secteur';
--- Tables/CONTRAINTS/tables.sql
+++ Tables/CONTRAINTS/tables.sql
@@ -15,7 +15,7 @@ DROP TABLE secteurs cascade constraints;
 
 DROP TABLE liste_secteurs cascade constraints;
 
-DROP TABLE BESIONS cascade constraints;
+DROP TABLE BESOINS cascade constraints;
 
 PROMPT DEleting existing sequence
 DROP sequence user_seq;
@@ -38,7 +38,7 @@ CREATE TABLE utilisateurs(
     numero VARCHAR(13 char),
     email VARCHAR(45 CHAR),
     mot_de_passe varchar(20 char),
-    status VARCHAR(15 CHAR),
+    statut VARCHAR(15 CHAR),
     profession VARCHAR(35 CHAR)
 );
 
@@ -47,7 +47,7 @@ CREATE sequence can_seq START WITH 1;
 CREATE TABLE candidatures(
     candidature_id integer DEFAULT can_seq.NEXTVAL NOT NULL,
     date_candidature DATE ,
-    status_candidature VARCHAR(15 CHAR),
+    statut_candidature VARCHAR(15 CHAR),
     proposition float(40),
     publication_id int,
     user_id int
@@ -60,7 +60,7 @@ publication_date DATE ,
 salaire float ,
 death_line VARCHAR(35),
 duree VARCHAR(20),
-status_publication VARCHAR(10 CHAR),
+statut_publication VARCHAR(10 CHAR),
 description VARCHAR(50 CHAR),
 besoin_id int
 );
@@ -68,8 +68,8 @@ besoin_id int
 
 PROMPT Creating table besoin_realise
 CREATE sequence be_rea_seq START WITH 1;
-CREATE TABLE besoin_realise(
-    besoin_realise_id integer DEFAULT be_rea_seq.NEXTVAL NOT NULL,
+CREATE TABLE besoin_realiser(
+    besoin_realiser_id integer DEFAULT be_rea_seq.NEXTVAL NOT NULL,
     note float ,
     commentaire VARCHAR( 50 CHAR),
     candidature_id int
@@ -80,20 +80,20 @@ PROMPT Creating table recommendation
 CREATE sequence recomm_seq START WITH 1;
 CREATE TABLE recommendations(
     recommendation_id integer DEFAULT recomm_seq.NEXTVAL NOT NULL,
-    recom_name VARCHAR(40 CHAR),
-    recom_number INT,
+    recom_nom VARCHAR(40 CHAR),
+    recom_numero INT,
     recom_email VARCHAR(30 CHAR),
     recom_contenue VARCHAR(50),
-    recom_status VARCHAR (33 CHAR),
+    recom_statut VARCHAR (33 CHAR),
     user_id integer
 );
 
 PROMPT Creating table secteurs
 CREATE TABLE secteurs(
     secteur_id VARCHAR(5),
-    secteur_name VARCHAR(25 CHAR),
-    secteyr_description VARCHAR( 30 CHAR),
-    number_applicant INT
+    secteur_nom VARCHAR(25 CHAR),
+    secteur_description VARCHAR( 30 CHAR),
+    nombre_utilisateur INT
 );
 
 PROMPT Creating table liste_secteur
@@ -106,10 +106,11 @@ CREATE TABLE liste_secteurs(
 
 PROMPT Creating table besion
 CREATE sequence bes_seq START WITH 1;
-CREATE TABLE BESIONS(
-    besion_id integer DEFAULT bes_seq.NEXTVAL NOT NULL,
+CREATE TABLE BESOINS(
+    besoin_id integer DEFAULT bes_seq.NEXTVAL NOT NULL,
     descrition VARCHAR (30 CHAR),
-    paiment_par VARCHAR (10 char),
+    paiement_par VARCHAR (10 char),
+    localisation VARCHAR (15 char),
     user_id int,
     secteur_id varchar(5)
 );
