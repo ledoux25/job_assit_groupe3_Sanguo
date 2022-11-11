a701e3815d331f5a35cbb998f18223f2f84207f8
Ledoux Sanguo <sanguojoseph17@gmail.com>
2022-11-11 23:44:54 +0100

    insertion dans la table secteur

--- Tables/CONTRAINTS/tables.sql
+++ Tables/CONTRAINTS/tables.sql
@@ -91,8 +91,8 @@ CREATE TABLE recommendations(
 PROMPT Creating table secteurs
 CREATE TABLE secteurs(
     secteur_id VARCHAR(5),
-    secteur_nom VARCHAR(25 CHAR),
-    secteur_description VARCHAR( 30 CHAR),
+    secteur_nom VARCHAR(22 CHAR),
+    secteur_description VARCHAR( 50 CHAR),
     nombre_utilisateur INT
 );
 
---
+++ Tables/secteur.sql
@@ -0,0 +1,22 @@
+
+PROMPT INSERTING into SECTEURS
+SET DEFINE OFF;
+begin
+INSERT INTO SECTEURS(secteur_id ,secteur_nom, secteur_description)VALUES('DOMES','Service Domestique','service interne(menagère)');--5
+INSERT INTO SECTEURS(secteur_id ,secteur_nom, secteur_description)VALUES('PLOMB','Plomberie', 'sevice lié a la plomberie');--2
+INSERT INTO SECTEURS(secteur_id ,secteur_nom, secteur_description)VALUES('JARDI','Jardinage', 'service lié a lentretien dun espace vert');--2
+INSERT INTO SECTEURS(secteur_id ,secteur_nom, secteur_description)VALUES('EDUCA','Education', 'les service lié a lapprentissage');--4
+INSERT INTO SECTEURS(secteur_id ,secteur_nom, secteur_description)VALUES('BATIM','Batiment' ,'serice lié au BTP (modification, construction)');--2
+INSERT INTO SECTEURS(secteur_id ,secteur_nom, secteur_description)VALUES('TECHN','Technicien', 'petit job technique (soudure,serrure)');
+INSERT INTO SECTEURS(secteur_id ,secteur_nom, secteur_description)VALUES('DESIN','Désinfection', 'service lié a la desinfection despace');
+INSERT INTO SECTEURS(secteur_id ,secteur_nom, secteur_description)VALUES('COUTU','Couture', 'service lié a la couture');--2
+INSERT INTO SECTEURS(secteur_id ,secteur_nom, secteur_description)VALUES('INTER','Design Interieur', 'service lie au design dinterieur');--3
+INSERT INTO SECTEURS(secteur_id ,secteur_nom, secteur_description)VALUES('ELECT','Electricité', 'service lie a la maintenance électrique');--4
+INSERT INTO SECTEURS(secteur_id ,secteur_nom, secteur_description)VALUES('RESTA','Restauration', 'service lié a lalimentation');--4
+INSERT INTO SECTEURS(secteur_id ,secteur_nom, secteur_description)VALUES('FROID','froid et climatisation', 'sevice lié au appareil froid ');--2
+INSERT INTO SECTEURS(secteur_id ,secteur_nom, secteur_description)VALUES('CONDU','Conduite', 'service lié a la conduite');--2
+INSERT INTO SECTEURS(secteur_id ,secteur_nom, secteur_description)VALUES('SECUR','securite', 'les service lié a la sécurité');--3
+end;
+/
+
+
