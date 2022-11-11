18ef66129bcad71c23c1ee1bb8f1e70834f33bf5
Ledoux Sanguo <sanguojoseph17@gmail.com>
2022-11-12 00:06:26 +0100

    creation du premier admin dans le fichier utlisateur

---
+++ Tables/utilisateur.sql
@@ -0,0 +1,7 @@
+
+PROMPT INSERTING into SECTEURS
+SET DEFINE OFF;
+begin
+INSERT INTO UTILISATEURS(nom, prenom, numero, email, mot_de_passe, statut, profession) VALUES ('Sanguo','Joseph','690-379-063','sanguojoseph1@gmail.com','admin','LIBRE',NULL);
+end;
+/
