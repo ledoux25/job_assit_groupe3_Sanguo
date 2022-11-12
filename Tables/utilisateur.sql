13e9de2f861e49292620629bfede290526a9dab3
Malonju <malonjulaure@gmail.com>
2022-11-12 23:36:10 +0100

    les modification sur sur les comment de la table besoins realiser

--- Tables/utilisateur.sql
+++ Tables/utilisateur.sql
@@ -1,7 +1,51 @@
 
-PROMPT INSERTING into SECTEURS
+PROMPT INSERTING into UTILISATEURS
 SET DEFINE OFF;
 begin
 INSERT INTO UTILISATEURS(nom, prenom, numero, email, mot_de_passe, statut, profession) VALUES ('Sanguo','Joseph','690-379-063','sanguojoseph1@gmail.com','admin','LIBRE',NULL);
+INSERT INTO UTILISATEURS(nom, prenom, numero, email, mot_de_passe, statut, profession) VALUES ('Atsangou','Darel','692-345-123','atsangoudarel23@gmail.com','LIBRE',NULL);
+INSERT INTO UTILISATEURS(nom, prenom, numero, email, mot_de_passe, statut, profession) VALUES ('Chiemo','patrice','656-239-003','Chiemopatrice03@gmail.com','0CCUPE',NULL);
+INSERT INTO UTILISATEURS(nom, prenom, numero, email, mot_de_passe, statut, profession) VALUES ('Awounang','stone','677-340-323','atsangoudarel23@gmail.com','BLOQUER',NULL);
+INSERT INTO UTILISATEURS(nom, prenom, numero, email, mot_de_passe, statut, profession) VALUES ('Bavoua','borelrel','657-840-103','Bavouaborel@gmail.com','LIBRE',NULL);
+
+INSERT INTO UTILISATEURS(nom, prenom, numero, email, mot_de_passe, statut, profession) VALUES ('Biyack','florent','659-245-023','Biyackflorentl3@gmail.com','LIBRE',NULL);
+INSERT INTO UTILISATEURS(nom, prenom, numero, email, mot_de_passe, statut, profession) VALUES ('Djouda','patric','690-367-143','Djoudapatric23@gmail.com','BlOQUER',NULL);
+INSERT INTO UTILISATEURS(nom, prenom, numero, email, mot_de_passe, statut, profession) VALUES ('Ekonke','preshly','677-445-193','Ebonkepreshly03@gmail.com','OCCUPER',NULL);
+
+INSERT INTO UTILISATEURS(nom, prenom, numero, email, mot_de_passe, statut, profession) VALUES ('Fotso','manuel','656-305-893','fotsomanuel@gmail.com','LIBRE',NULL);
+INSERT INTO UTILISATEURS(nom, prenom, numero, email, mot_de_passe, statut, profession) VALUES ('Jidjou','audrey','692-345-403','Djidjouaudrey30@gmail.com','OCCUPER',NULL);
+INSERT INTO UTILISATEURS(nom, prenom, numero, email, mot_de_passe, statut, profession) VALUES ('Josue','nelson','692-345-123','Josuenelson45@gmail.com','LIBRE',NULL);
+INSERT INTO UTILISATEURS(nom, prenom, numero, email, mot_de_passe, statut, profession) VALUES ('Kangue','loic','694-333-003','Kangueloic33@gmail.com','BLOQUER',NULL);
+
+INSERT INTO UTILISATEURS(nom, prenom, numero, email, mot_de_passe, statut, profession) VALUES ('Kanteu','maxime','694-045-333','Kanteumaxime3@gmail.com','BLOQUER',NULL);
+INSERT INTO UTILISATEURS(nom, prenom, numero, email, mot_de_passe, statut, profession) VALUES ('keneKenne','paulin','698-345-523','kenepauelin002@gmail.com','BLOQUER',NULL);
+INSERT INTO UTILISATEURS(nom, prenom, numero, email, mot_de_passe, statut, profession) VALUES ('Kenchoun','dany','692-345-013','Kenchoumdany@gmail.com','LIBRE',NULL);
+
+INSERT INTO UTILISATEURS(nom, prenom, numero, email, mot_de_passe, statut, profession) VALUES ('Kengni','johan','692-345-123','Kegnijohan23@gmail.com','LIBRE',NULL);
+INSERT INTO UTILISATEURS(nom, prenom, numero, email, mot_de_passe, statut, profession) VALUES ('Lontsi','hernandez','692-345-123','Lontsihernandez05@gmail.com','BLOQUER',NULL);
+INSERT INTO UTILISATEURS(nom, prenom, numero, email, mot_de_passe, statut, profession) VALUES ('Makolo','megane','692-345-123','Makolomegane40@gmail.com','OCCUPER',NULL);
+INSERT INTO UTILISATEURS(nom, prenom, numero, email, mot_de_passe, statut, profession) VALUES ('Malonju','laure','655-205-137','Malonjulaure1@gmail.com','OCCUPER',NULL);
+
+INSERT INTO UTILISATEURS(nom, prenom, numero, email, mot_de_passe, statut, profession) VALUES ('Manga','rebecca','690-045-122','Mangarebecca12@gmail.com','OCCUPER',NULL);
+INSERT INTO UTILISATEURS(nom, prenom, numero, email, mot_de_passe, statut, profession) VALUES ('Mbah Makam','jenny','798-345-143','Mbahjenny10@gmail.com','BLOQUER',NULL);
+INSERT INTO UTILISATEURS(nom, prenom, numero, email, mot_de_passe, statut, profession) VALUES ('Motso','alix','694-345-120','Motsoalix60@gmail.com','OCCUPER',NULL);
+INSERT INTO UTILISATEURS(nom, prenom, numero, email, mot_de_passe, statut, profession) VALUES ('Mouofo','danella','693-305-103','mouofodanella02@gmail.com','LIBRE',NULL);
+INSERT INTO UTILISATEURS(nom, prenom, numero, email, mot_de_passe, statut, profession) VALUES ('Naa Bieleu','roussel','690-305-123','naaroussel3@gmail.com','LIBRE',NULL);
+
+INSERT INTO UTILISATEURS(nom, prenom, numero, email, mot_de_passe, statut, profession) VALUES ('Nguenou','wilfried','677-345-023','Ngeunouwilfried02@gmail.com','LIBRE',NULL);
+INSERT INTO UTILISATEURS(nom, prenom, numero, email, mot_de_passe, statut, profession) VALUES ('Nguiakam','angel','679-345-300','Nguiakamangel33@gmail.com','LIBRE',NULL);
+INSERT INTO UTILISATEURS(nom, prenom, numero, email, mot_de_passe, statut, profession) VALUES ('Nikiledji','aurore','670-305-000','Nikiledjiaurore00@gmail.com','LIBRE',NULL);
+INSERT INTO UTILISATEURS(nom, prenom, numero, email, mot_de_passe, statut, profession) VALUES ('Njaha','nathan','671-375-063','Njahanathan55@gmail.com','OCCUPER',NULL);
+INSERT INTO UTILISATEURS(nom, prenom, numero, email, mot_de_passe, statut, profession) VALUES ('Njopmo','loic','698-345-773','Ndjomoloic40@gmail.com','BLOQUER',NULL);
+
+INSERT INTO UTILISATEURS(nom, prenom, numero, email, mot_de_passe, statut, profession) VALUES ('Nono','jordan','692-345-333','Nonojordan11@gmail.com','BLOQUER',NULL);
+INSERT INTO UTILISATEURS(nom, prenom, numero, email, mot_de_passe, statut, profession) VALUES ('Ntanguen','evrard','692-345-123','Ntanguenevrard@gmail.com','LIBRE',NULL);
+INSERT INTO UTILISATEURS(nom, prenom, numero, email, mot_de_passe, statut, profession) VALUES ('Nzomo','rabier','692-345-903','Nzomorabier@gmail.com','LIBRE',NULL);
+INSERT INTO UTILISATEURS(nom, prenom, numero, email, mot_de_passe, statut, profession) VALUES ('Peughouia','fred','678-345-202','Peughouiafred@gmail.com','OCCUPER',NULL);
+INSERT INTO UTILISATEURS(nom, prenom, numero, email, mot_de_passe, statut, profession) VALUES ('pokem','maxime','690-777-999','pokemmaxime@gmail.com','BLOQUER',NULL);
+
+INSERT INTO UTILISATEURS(nom, prenom, numero, email, mot_de_passe, statut, profession) VALUES ('Pouani','justin','655-205-137','Pouanijustin2@gmail.com','OCCUPER',NULL);
+INSERT INTO UTILISATEURS(nom, prenom, numero, email, mot_de_passe, statut, profession) VALUES ('Raye','malik','655-205-137','Rayemalik11@gmail.com','OCCUPER',NULL);
+INSERT INTO UTILISATEURS(nom, prenom, numero, email, mot_de_passe, statut, profession) VALUES ('Sado','scott','655-205-137','sadoscott9@gmail.com','LIBRE',NULL);
 end;
 /
