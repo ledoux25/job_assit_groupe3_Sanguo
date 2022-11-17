bed7fd6157cb1575570d79f2b823777e21e472e9
Patrice <Wapalu2004@gmail.com>
2022-11-17 17:06:20 +0100

    Mise à jour de la table besoins

--- Tables/besoins.sql
+++ Tables/besoins.sql
@@ -5,78 +5,78 @@ INSERT INTO BESOINS(description,paiement_par,localisation)
 VALUES('besoins d une menagere','mois','douala');
 
 INSERT INTO BESOINS(description,paiement_par,localisation)
-VALUES('beoins d un plombier ','jour','douala');
+VALUES('besoins d un plombier ','jour','douala');
 
 INSERT INTO BESOINS(description,paiement_par,localisation)
-VALUES('beoins d un mecanisien ','jour','yaounde');
+VALUES('besoins d un mecanisien ','jour','yaounde');
 
 INSERT INTO BESOINS(description,paiement_par,localisation)
-VALUES('beoins d une nounou','semaine','yaounde');
-
-
-INSERT INTO BESOINS(description,paiement_par,localisation)
-VALUES('beoins d une baby-sitter','semaine','buea'); BESOINS
+VALUES('besoins d une nounou','semaine','yaounde');
 
 
 INSERT INTO BESOINS(description,paiement_par,localisation)
-VALUES('beoins d un jardinier','jour','bafoussam');
+VALUES('besoins d une baby-sitter','semaine','buea');
 
 
 INSERT INTO BESOINS(description,paiement_par,localisation)
-VALUES('beoins d un repetiteur ','jour','bamenda');
+VALUES('besoins d un jardinier','jour','bafoussam');
 
 
 INSERT INTO BESOINS(description,paiement_par,localisation)
-VALUES('beoins d un plombier','jour','yaounde'); BESOINS
+VALUES('besoins d un repetiteur ','jour','bamenda');
 
 
 INSERT INTO BESOINS(description,paiement_par,localisation)
-VALUES('beoins d une couturiere ','semaine','bafan');
+VALUES('besoins d un plombier','jour','yaounde');
 
 
 INSERT INTO BESOINS(description,paiement_par,localisation)
-VALUES('beoins d un fermier ','mois','douala');
+VALUES('besoins d une couturiere ','semaine','bafan');
 
 
 INSERT INTO BESOINS(description,paiement_par,localisation)
-VALUES('beoins d un mecanisien ','jour','yaounde');
+VALUES('besoins d un fermier ','mois','douala');
 
 
 INSERT INTO BESOINS(description,paiement_par,localisation)
-VALUES('beoins d un cuisinier ','semaine','bafan');
+VALUES('besoins d un mecanisien ','jour','yaounde');
 
 
 INSERT INTO BESOINS(description,paiement_par,localisation)
-VALUES('beoins d un menusier','jour','edea');
+VALUES('besoins d un cuisinier ','semaine','bafan');
 
 
 INSERT INTO BESOINS(description,paiement_par,localisation)
-VALUES('beoins d un serurier ','jour','douala');
+VALUES('besoins d un menusier','jour','edea');
 
 
 INSERT INTO BESOINS(description,paiement_par,localisation)
-VALUES('beoins d une menagere','semaine','yaounde');
+VALUES('besoins d un serrurier ','jour','douala');
+
 
 INSERT INTO BESOINS(description,paiement_par,localisation)
-VALUES('beoins d une couturiere','jour','yaounde'); 
+VALUES('besoins d une menagere','semaine','yaounde');
 
 INSERT INTO BESOINS(description,paiement_par,localisation)
-VALUES('beoins d un repetieur ','semaine','fouban'); 
+VALUES('besoins d une couturiere','jour','yaounde'); 
 
 INSERT INTO BESOINS(description,paiement_par,localisation)
-VALUES('beoins d un electricien','jour','douala'); 
+VALUES('besoins d un repetieur ','semaine','fouban'); 
 
 INSERT INTO BESOINS(description,paiement_par,localisation)
-VALUES('beoins d un  charpantier','jour','yaounde'); 
+VALUES('besoins d un electricien','jour','douala'); 
 
 INSERT INTO BESOINS(description,paiement_par,localisation)
-VALUES('beoins d un frigoriste','jour','edea'); 
+VALUES('besoins d un  charpantier','jour','yaounde'); 
 
 INSERT INTO BESOINS(description,paiement_par,localisation)
-VALUES('beoins d un plombier','jour','yaounde'); 
+VALUES('besoins d un frigoriste','jour','edea'); 
 
 INSERT INTO BESOINS(description,paiement_par,localisation)
-VALUES('beoins d un plombier','jour','yaounde'); BESOINS
+VALUES('besoins d un plombier','jour','yaounde'); 
+
+INSERT INTO BESOINS(description,paiement_par,localisation)
+VALUES('besoins d un plombier','jour','yaounde');
 
 end;
 /
