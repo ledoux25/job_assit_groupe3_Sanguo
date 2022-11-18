335f62b7a76ffbeaf57b7bf3395819368816a0a2
ledoux <sanguojoseph17@gmail.com>
2022-11-18 01:12:37 +0100

    corretion des insertion du fichier reomendation et utilisateur (la casse)

--- Tables/CONTRAINTS/tables.sql
+++ Tables/CONTRAINTS/tables.sql
@@ -81,7 +81,7 @@ CREATE sequence recomm_seq START WITH 1;
 CREATE TABLE recommendations(
     recommendation_id integer DEFAULT recomm_seq.NEXTVAL NOT NULL,
     recom_nom VARCHAR(40 CHAR),
-    recom_numero INT, 
+    recom_numero VARCHAR(13 char), 
     recom_email VARCHAR(30 CHAR),
     recom_contenue VARCHAR(50),
     recom_statut VARCHAR (33 CHAR),
--- Tables/utilisateur.sql
+++ Tables/utilisateur.sql
@@ -3,18 +3,18 @@ PROMPT INSERTING into UTILISATEURS
 SET DEFINE OFF;
 begin
 
-INSERT INTO utilisateurs(user_id, prenom, numero, email, mot_de_passe, statut) VALUES (1,'Sanguo','Joseph','690375863','sanguojoseph5@gmail.com','admin','LIBRE','menagere');--1
-INSERT INTO UTILISATEURS(user_id, prenom, numero, email, mot_de_passe, statut) VALUES (2,'Atsangou','Darel','692-345-123','atsangoudarel23@gmail.com','atsa','LIBRE','plombier');--2
-INSERT INTO UTILISATEURS(user_id, prenom, numero, email, mot_de_passe, statut) VALUES (3,'Chiemo','patrice','656-239-003','Chiemopatrice03@gmail.com','chie','0CCUPE','couturiere');
-INSERT INTO UTILISATEURS(user_id, prenom, numero, email, mot_de_passe, statut) VALUES (4,'Awounang','stone','677-340-323','aowounangstonel23@gmail.com','awou','BLOQUER','jardinier');--4
-INSERT INTO UTILISATEURS(user_id, prenom, numero, email, mot_de_passe, statut) VALUES (5,'Chiemo','patrice','656-239-003','Chiemopatrice03@gmail.com','chie','OCCUPER','couturiere');--5
-INSERT INTO UTILISATEURS(user_id, prenom, numero, email, mot_de_passe, statut) VALUES (6,'Awounang','stone','677-340-323','aowounangstonel23@gmail.com','awou','BLOQUER','jardininier');--6
-INSERT INTO UTILISATEURS(user_id, prenom, numero, email, mot_de_passe, statut) VALUES (7,'Bavoua','borel','657-840-103','Bavouaborel@gmail.com','bavou','LIBRE','homme de chambre');--6
-INSERT INTO UTILISATEURS(user_id, prenom, numero, email, mot_de_passe, statut) VALUES (8,'Biyack','florent','659-245-023','Biyackflorentl3@gmail.com','biyac','LIBRE','baby-setting');--7
-INSERT INTO UTILISATEURS(user_id, prenom, numero, email, mot_de_passe, statut) VALUES (9,'Djouda','patric','690-367-143','Djoudapatric23@gmail.com','djou','BLOQUER','cousier');--8
-INSERT INTO UTILISATEURS(user_id, prenom, numero, email, mot_de_passe, statut) VALUES (10,'Ekonke','preshly','677-445-193','Ebonkepreshly03@gmail.com','ekon','OCCUPER','decorateur');--9
-INSERT INTO UTILISATEURS(user_id, prenom, numero, email, mot_de_passe, statut) VALUES (11,'Fotso','manuel','656-305-893','fotsomanuel@gmail.com','fots','LIBRE','decoratrice');--10
-INSERT INTO UTILISATEURS(user_id, prenom, numero, email, mot_de_passe, statut) VALUES (12'Jidjou','audrey','692-345-403','Djidjouaudrey30@gmail.com','djid','OCCUPER','peintre');--11
+INSERT INTO utilisateurs(user_id, prenom, numero, email, mot_de_passe, statut) VALUES (1,'Sanguo','Joseph','690'375'863','sanguojoseph5@gmail.com','admin','LIBRE');--1
+INSERT INTO UTILISATEURS(user_id, prenom, numero, email, mot_de_passe, statut) VALUES (2,'Atsangou','Darel','692-345-123','atsangoudarel23@gmail.com','atsa','LIBRE');--2
+INSERT INTO UTILISATEURS(user_id, prenom, numero, email, mot_de_passe, statut) VALUES (3,'Chiemo','Patrice','656-239-003','Chiemopatrice03@gmail.com','chie','0CCUPE');
+INSERT INTO UTILISATEURS(user_id, prenom, numero, email, mot_de_passe, statut) VALUES (4,'Awounang','Ptone','677-340-323','aowounangstonel23@gmail.com','awou','BLOQUER');--4
+INSERT INTO UTILISATEURS(user_id, prenom, numero, email, mot_de_passe, statut) VALUES (5,'Chiemo','Patrice','656-239-003','Chiemopatrice03@gmail.com','chie','OCCUPER');--5
+INSERT INTO UTILISATEURS(user_id, prenom, numero, email, mot_de_passe, statut) VALUES (6,'Awounang','Stone','677-340-323','aowounangstonel23@gmail.com','awou','BLOQUER');--6
+INSERT INTO UTILISATEURS(user_id, prenom, numero, email, mot_de_passe, statut) VALUES (7,'Bavoua','Borel','657-840-103','Bavouaborel@gmail.com','bavou','LIBRE');--6
+INSERT INTO UTILISATEURS(user_id, prenom, numero, email, mot_de_passe, statut) VALUES (8,'Biyack','Florent','659-245-023','Biyackflorentl3@gmail.com','biyac','LIBRE');--7
+INSERT INTO UTILISATEURS(user_id, prenom, numero, email, mot_de_passe, statut) VALUES (9,'Djouda','Patric','690-367-143','Djoudapatric23@gmail.com','djou','BLOQUER');--8
+INSERT INTO UTILISATEURS(user_id, prenom, numero, email, mot_de_passe, statut) VALUES (10,'Ekonke','Preshly','677-445-193','Ebonkepreshly03@gmail.com','ekon','OCCUPER');--9
+INSERT INTO UTILISATEURS(user_id, prenom, numero, email, mot_de_passe, statut) VALUES (11,'Fotso','Manuel','656-305-893','fotsomanuel@gmail.com','fots','LIBRE');--10
+INSERT INTO UTILISATEURS(user_id, prenom, numero, email, mot_de_passe, statut) VALUES (12'Jidjou','Audrey','692-345-403','Djidjouaudrey30@gmail.com','djid','OCCUPER');--11
 end;
 /
 
