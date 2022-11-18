a575aedea7fa339e0c14cfec2834f0eb885866a1
kanteumaxime <anangamax@gmail.com>
2022-11-18 15:45:23 +0100

    insertion dans la table besoin_realise

---
+++ OBJECTS/DATA/besions_realiser.sql
@@ -0,0 +1,39 @@
+PROMPT INSERTING into besoin_realiser
+SET DEFINE OFF;
+begin
+INSERT INTO besoin_realiser(besoins_realiser_id,note,commentaire,candidature_id)
+VALUES (1,12.5,'un peux paresseux',1);--1
+
+INSERT INTO besoin_realiser(besoins_realiser_id,note,commentaire,candidature_id)
+VALUES (2,12.5,'trop discret',2);--2
+
+INSERT INTO besoin_realiser(besoins_realiser_id,note,commentaire,candidature_id)
+VALUES (3,5,'il vient toujour en retard',3);--3
+
+
+INSERT INTO besoin_realiser(besoins_realiser_id,note,commentaire,candidature_id)
+VALUES (4,15.5,'tres travalleurs',4);--4
+
+INSERT INTO besoin_realiser(besoins_realiser_id,note,commentaire,candidature_id)
+VALUES (5,18.5,'tres responsable et travailleur',5);--5
+
+INSERT INTO besoin_realiser(besoins_realiser_id,note,commentaire,candidature_id)
+VALUES (6,10.5,'un peu meprisant',6);--6
+
+INSERT INTO besoin_realiser(besoins_realiser_id,note,commentaire,candidature_id)
+VALUES (7,14.5,'prend tres bien soin des enfants',7);--7
+
+INSERT INTO besoin_realiser(besoins_realiser_id,note,commentaire,candidature_id)
+VALUES (8,3.5,'ne connais pas cuisiner',8);--8
+
+INSERT INTO besoin_realiser(besoins_realiser_id,note,commentaire,candidature_id)
+VALUES (9,12.5,'il na pas respecter son rende-vous',9);--9
+
+INSERT INTO besoin_realiser(besoins_realiser_id,note,commentaire,candidature_id)
+VALUES (10,12,'il passe son temps a dormir ',10);--10
+
+INSERT INTO besoin_realiser(besoins_realiser_id,note,commentaire,candidature_id)
+VALUES (11,15,'tres bon ce gar',11);--11
+
+INSERT INTO besoin_realiser(besoins_realiser_id,note,commentaire,candidature_id)
+VALUES (12,0.5,'neglige mes enfants',12);--12
